f7b685b4f262d2b10a7ecc134023e2175d02af77
Isabelle Bertram <isabellebertram@Isabelles-MacBook-Air.local>
2021-05-09 18:24:59 -0500

    added ability to add thoughts

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,25 +1,44 @@
 import React from 'react';
 import { Redirect, useParams } from 'react-router-dom';
 
+import ThoughtForm from '../components/ThoughtForm';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
-import ThoughtForm from '../components/ThoughtForm';
-import Auth from '../utils/auth';
-
 
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import { ADD_FRIEND } from '../utils/mutations';
+import Auth from '../utils/auth';
 
-const Profile = () => {
-  const [addFriend] = useMutation(ADD_FRIEND);
+const Profile = props => {
   const { username: userParam } = useParams();
 
+  const [addFriend] = useMutation(ADD_FRIEND);
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam }
   });
 
   const user = data?.me || data?.user || {};
+
+  // redirect to personal profile page if username is yours
+  if (
+    Auth.loggedIn() &&
+    Auth.getProfile().data.username === userParam
+  ) {
+    return <Redirect to="/profile" />;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this. Use the navigation links above to sign up or log in!
+      </h4>
+    );
+  }
 
   const handleClick = async () => {
     try {
@@ -31,27 +50,13 @@ const Profile = () => {
     }
   };
 
-  // redirect to personal profile page if username is the logged-in user's
-  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-    return <Redirect to="/profile" />;
-  }
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-  if (!user?.username) {
-    return (
-      <h4>
-        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
-      </h4>
-    );
-  }
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
+
         {userParam && (
           <button className="btn ml-auto" onClick={handleClick}>
             Add Friend
--- server/server.js
+++ server/server.js
@@ -1,31 +1,25 @@
 const express = require('express');
-const path = require('path');
-// import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 
-// import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
-
 const { authMiddleware } = require('./utils/auth');
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-// create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware
 });
 
-// Serve up static assets
-
-// integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
@@ -37,7 +31,6 @@ app.get('*', (req, res) => {
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
